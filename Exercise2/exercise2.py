1bcdb14578a40dd41400fb8040d5e69ca9e800ec
RatBaugette <pleasepeople123@gmail.com>
2023-10-31 15:18:30 +0800

    Include Result for lab3

--- Exercise2/exercise2.py
+++ Exercise2/exercise2.py
@@ -1,16 +1,21 @@
 def calculate_bmi(height, weight):
+    result = 0.0
     print("Height = " + str(height))
     print("Weight = " + str(weight))
     height = float(height)
     weight = float(weight)
     bmi = float(weight/(height*height))
-    return bmi
+    if bmi < 18.5:
+        print("Underweight")
+        result = -1
+    elif 18.5 <= bmi <= 25.0:
+        print("Normal Weight")
+        result = 0
+    else:
+        print("Overweight")
+        result = 1
+    print(result)
+    return result
 
 
-bmi = calculate_bmi(weight=57, height=1.73)
-if bmi < 18.5:
-    print("Underweight")
-elif 18.5 <= bmi <= 25.0:
-    print("Normal Weight")
-else:
-    print("Overweight")
+result = calculate_bmi(weight=57, height=1.73)
